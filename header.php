0b592a39f731c3bf0503d9af53af42a6116652a2
ardianjatip <ardian.jati@ui.ac.id>
2017-05-05 21:33:46 +0700

    header

--- header.php
+++ header.php
@@ -5,6 +5,16 @@
     <title></title>
     <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+  <link rel="stylesheet" type="text/css" href="/src/css/header.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,12 +23,10 @@
   <header>
     <nav>
    <div class="nav-wrapper">
-     <a href="#!" class="brand-logo"><img src="src/image/Sirima UI.png" alt="">SIRIMA</a>
+     <a href="#!" class="brand-logo"><img src="src/image/Sirima UI.png" alt=""><span>SIRIMA</span></a>
      <ul class="right hide-on-med-and-down">
-       <li><a href="#"><i class="material-icons">search</i></a></li>
-       <li><a href="#"><i class="material-icons">view_module</i></a></li>
-       <li><a href="#"><i class="material-icons">refresh</i></a></li>
-       <li><a href="#"><i class="material-icons">more_vert</i></a></li>
+       <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+       <li><a href="#"><span class="glyphicon glyphicon-log-out"></span></a></li>
      </ul>
    </div>
  </nav>
