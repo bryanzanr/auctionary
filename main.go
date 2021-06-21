aa4f9918f502e463e3a47e70368e1f9d846910b0
bryanzanr <bryanzanr@gmail.com>
2021-06-21 18:30:02 +0700

    Another template from heroku but using gin

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	// "log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		// log.Fatal("$PORT must be set")
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+}
